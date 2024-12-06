c236675117d8423a09262ea007839e39d0925de3
Final Project <jenkins@theconstruct.ai>
2024-12-06 10:44:37 +0900

    fine tune & Integration testing #7-241206

--- local_start_jenkins.sh
+++ local_start_jenkins.sh
@@ -9,14 +9,13 @@ set -e
 export JENKINS_HOME=~/final_project/webpage_ws/.jenkins
 mkdir -p $JENKINS_HOME
 
-# Install java. We are using JRE 17.
-sudo apt-get update -y || true
-sudo apt-get install -y openjdk-17-jre
-
 # Download the Jenkins .war file, if not there already
 cd ~/final_project/webpage_ws/.jenkins
 JENKINS_FILE="/home/invokelee/final_project/webpage_ws/.jenkins/jenkins.war"
 if [ ! -f "$JENKINS_FILE" ]; then
+# Install java. We are using JRE 17.
+    sudo apt-get update -y || true
+    sudo apt-get install -y openjdk-17-jre
     wget https://updates.jenkins.io/download/war/2.430/jenkins.war
 fi
 
@@ -32,15 +31,14 @@ else
     # Run Jenkins with a prefix. A prefix is needed because we are using a 
     # reverse proxy to run it on the Academy. This may not be necessary in your setup.
     # Store Jenkins proceess ID in JENKINS_PID
-    java -jar jenkins.war --prefix="/$SLOT_PREFIX/jenkins/" &
+    java -jar jenkins.war --prefix="/jenkins/" &
     JENKINS_PID=$!
     sleep 15s
 
     # Calculate the Jenkins proxy address
     # This is NOT required for running Jenkins. It's just something we
     # need to do on the Academy. INSTANCE_ID and SLOT_PREFIX are Academy variables.
-    INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-    URL=`echo "https://$INSTANCE_ID.robotigniteacademy.com/$SLOT_PREFIX/jenkins/"`
+    URL=`echo "https://localhost:8080/jenkins/"`
     echo ""
     echo "1. Jenkins is running in the background."
     echo "2. Switch to the webpage tab to see it."
